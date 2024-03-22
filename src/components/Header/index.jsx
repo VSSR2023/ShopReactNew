89d1a36f6aeda4ae7b0f04e16cfc1568e93bf26d
VSSR2023 <135501916+VSSR2023@users.noreply.github.com>
2024-03-22 20:55:52 +0400

    change props to useContext

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,9 +2,38 @@
 import styles from "./Header.module.scss"
 import React, { useState } from "react";
 import { SlBasket } from "react-icons/sl";
+import Orders from "./../Orders";
+import {useAppContext} from "./../../useAppContext"
 
 export default function Header(){
+    const {orders, deleteOrder} = useAppContext();
+
+
     let [cartOpen,setCartOpen]=useState(false);
+
+
+
+    const showOrders=()=>{
+        let summa=0;
+        orders.forEach(el=>summa+=Number.parseFloat(el.price));
+        return(
+            <div>
+                {orders.map(el=>(
+                    <Orders onDelete={deleteOrder} key={el.id} item={el}/>
+                ))}
+                <p className={styles.summa}>Итого:{new Intl.NumberFormat().format(summa)} $</p>
+            </div>
+        );
+    }
+    const showNothing=()=>{
+        return(
+            <div className={styles.empty}>
+                <h2>Товары отсутствуют в карзине</h2>
+            </div>
+        );
+    }
+
+
     return(
         <header>
             <div>
@@ -14,14 +43,18 @@ export default function Header(){
                     <li>Контакты</li>
                     <li>Личный кабинет</li>
                 </ul>
-                 <SlBasket onClick={()=>setCartOpen(cartOpen= !cartOpen)} className={`${styles.shopCartButton} ${cartOpen ? styles.active : ''}`}/>
+                <SlBasket onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`${styles.shopCartButton} ${cartOpen ? styles.active : ''}`} />
 
-                {cartOpen &&(
+                 {cartOpen && (
                     <div className={styles.shopCart}>
+                        {orders.length>0?
+                            showOrders():showNothing()
+                    }
 
                     </div>
                 )}
             </div>
+
             <div className={styles.presentation}></div>
         </header>
     );
--- src/components/Orders/index.jsx
+++ src/components/Orders/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./Orders.module.scss";
+import { TiDeleteOutline } from "react-icons/ti";
+import {useAppContext} from "./../../useAppContext";
 
-export default function Orders(props){
+export default function Orders({item}){
+
+    const {deleteOrder} = useAppContext();
+
     return(
-        <></>
+        <div className={styles.item}>
+             <img src={"./images/"+item.img} alt="Error" />
+            <h2>{item.title}</h2>
+            <b>{item.price} руб. </b>
+            <TiDeleteOutline onClick={()=>deleteOrder(item.id)} className={styles.deleteIcon} />
+        </div>
     );
 }
